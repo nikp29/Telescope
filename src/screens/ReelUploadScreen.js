75d697215627109fb0c0bb56a629c06933dec6b4
Nikhil Patel <nhpatel@usc.edu>
2021-04-02 10:50:42 -0700

    checks database before  confirm upload

--- src/screens/ReelUploadScreen.js
+++ src/screens/ReelUploadScreen.js
@@ -16,25 +16,55 @@ const ReelUploadScreen = () => {
         submitButtonText="Upload"
         onSubmit={async ({ url, title, tags }) => {
           const usersRef = firebase.firestore().collection("users");
-          const uid = AsyncStorage.getItem("token");
-          const data = await usersRef.doc(uid).get();
-          if (!data) {
-            setError("Unable to get user information");
-          } else if (data.hasOwnProperty("lastUploaded")) {
-            // convert unix timestamp to moment
-            const lastUploaded = moment.unix(data.lastUploaded);
-            const current = moment().subtract(1, "d").unix(); // limit 1 upload per day
-            if (current > lastUploaded) {
-              // if more than 24 hours has elapsed
-              navigate("ConfirmUpload");
-            } else {
-              setError(
-                "You must wait 24 hours since your last post before posting a new reel"
-              );
-            }
-          } else {
-            navigate("ConfirmUpload");
-          }
+          const uid = await AsyncStorage.getItem("token");
+          console.log(uid);
+          await usersRef
+            .doc(uid)
+            .get()
+            .then((firestoreDocument) => {
+              if (!firestoreDocument.exists) {
+                setError("Error getting user information");
+                return;
+              }
+              const data = firestoreDocument.data();
+              console.log(data);
+              if (data.hasOwnProperty("lastUploaded")) {
+                // convert unix timestamp to moment
+                const lastUploaded = moment.unix(data.lastUploaded);
+                const current = moment().subtract(1, "d").unix(); // limit 1 upload per day
+                if (current > lastUploaded) {
+                  // if more than 24 hours has elapsed
+                  navigate("ConfirmUpload");
+                } else {
+                  setError(
+                    "You must wait 24 hours since your last post before posting a new reel"
+                  );
+                }
+              } else {
+                navigate("ConfirmUpload");
+              }
+            })
+            .catch((error) => {
+              setError(error.message);
+            });
+          // console.log(data);
+          // if (!data) {
+          //   setError("Unable to get user information");
+          // } else if (data.hasOwnProperty("lastUploaded")) {
+          //   // convert unix timestamp to moment
+          //   const lastUploaded = moment.unix(data.lastUploaded);
+          //   const current = moment().subtract(1, "d").unix(); // limit 1 upload per day
+          //   if (current > lastUploaded) {
+          //     // if more than 24 hours has elapsed
+          //     navigate("ConfirmUpload");
+          //   } else {
+          //     setError(
+          //       "You must wait 24 hours since your last post before posting a new reel"
+          //     );
+          //   }
+          // } else {
+          //   navigate("ConfirmUpload");
+          // }
         }}
       />
     </View>
