d0b7680d078a737c883667ca342afecf3f756d4f
Nikhil Patel <nhpatel@usc.edu>
2021-04-02 10:31:10 -0700

    fixed oauth bug, begun work on uploading videos

---
+++ src/screens/ConfirmUploadScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const ConfirmUploadScreen = ({ navigation }) => {
+  return <Text style={{ fontSize: 48 }}>ConfirmUploadScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ConfirmUploadScreen;
--- src/screens/ReelUploadScreen.js
+++ src/screens/ReelUploadScreen.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import UploadForm from "./../components/UploadForm";
+import { navigate } from "../navigationRef";
+import AsyncStorage from "@react-native-community/async-storage";
+import { firebase } from "../firebase/config.js";
+import moment from "moment";
 
 const ReelUploadScreen = () => {
-  return <Text style={{ fontSize: 48 }}>ReelUploadScreen</Text>;
+  const [error, setError] = useState("");
+  return (
+    <View style={styles.container}>
+      <UploadForm
+        headerText="Upload Your Reel"
+        errorMessage={error}
+        submitButtonText="Upload"
+        onSubmit={async ({ url, title, tags }) => {
+          const usersRef = firebase.firestore().collection("users");
+          const uid = AsyncStorage.getItem("token");
+          const data = await usersRef.doc(uid).get();
+          if (!data) {
+            setError("Unable to get user information");
+          } else if (data.hasOwnProperty("lastUploaded")) {
+            // convert unix timestamp to moment
+            const lastUploaded = moment.unix(data.lastUploaded);
+            const current = moment().subtract(1, "d").unix(); // limit 1 upload per day
+            if (current > lastUploaded) {
+              // if more than 24 hours has elapsed
+              navigate("ConfirmUpload");
+            } else {
+              setError(
+                "You must wait 24 hours since your last post before posting a new reel"
+              );
+            }
+          } else {
+            navigate("ConfirmUpload");
+          }
+        }}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+ReelUploadScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default ReelUploadScreen;
