e86f83858d1e869460464b2bcf0a32f7a0e91a9c
Nikhil Patel <nhpatel@usc.edu>
2021-04-13 22:07:49 -0700

    small fixes

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -159,18 +159,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   name: {
-    fontFamily: "Raleway",
+    fontFamily: "Raleway-Bold",
     fontSize: 32,
     fontWeight: "bold",
   },
   bio: {
-    fontFamily: "Raleway",
+    fontFamily: "Raleway-Regular",
     fontSize: 16,
     color: "#545454",
     fontWeight: "normal",
   },
   reels: {
-    fontFamily: "Raleway",
+    fontFamily: "Raleway-Regular",
     fontSize: 14,
     fontWeight: "normal",
   },
